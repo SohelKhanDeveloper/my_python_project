a59b3dc87451995718bfa4409372d5c41784ea01
sohel khan <sohelcse32@gmail.com>
2025-09-22 01:04:42 -0400

    commit test project

---
+++ Test.py
@@ -0,0 +1,18 @@
+# First Project
+
+print("Hello world....")
+
+a=21
+b=2
+sum=a + b
+sub=a-b
+mult=a*b
+div=a/b
+no_Decimal_div=a//b
+print("a=21 and b=2 resuls are showing bellow")
+print("Adition:", sum)
+print("Substraction:",sub)
+print("multiplictaion:",mult)
+print("division:",div)
+print("Round Number:",no_Decimal_div)
+
