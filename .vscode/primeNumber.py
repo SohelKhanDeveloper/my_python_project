5019d2109d6aefc9a1b5517778bf18d7bd5d7fed
sohelkhan <sohelcse32@gmail.com>
2025-09-27 16:32:22 -0400

    change

--- .vscode/primeNumber.py
+++ .vscode/primeNumber.py
@@ -1,5 +1,5 @@
-maxNum=int(input("Enter the maximum number: "))
 minNum=int(input("Enter the minimum number: "))
+maxNum=int(input("Enter the maximum number: "))
 for num in range(minNum,maxNum+1):  
    if num > 1:  
        for i in range(2,num):  
