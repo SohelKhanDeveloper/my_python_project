e6a5e9344a191779267de717d60adec825a74b9e
sohelkhan <sohelcse32@gmail.com>
2025-09-27 16:21:35 -0400

    commit primenumber

---
+++ .vscode/primeNumber.py
@@ -0,0 +1,9 @@
+maxNum=int(input("Enter the maximum number: "))
+minNum=int(input("Enter the minimum number: "))
+for num in range(minNum,maxNum+1):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           print(num)
